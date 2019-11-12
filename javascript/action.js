c7a8b26a23f654e748c1b95ad2a277840f39e575
benknight <ben.knight2336@gmail.com>
2019-11-12 12:32:23 -0700

    I split the scss files into media query's to make it easier to find code. Also Added api images to the page

--- javascript/action.js
+++ javascript/action.js
@@ -1,37 +1,49 @@
-let numPlayers = 5;
+let numPlayers=1;
 let frontNine = 9;
 let backNine = 18;
 
+function enterPlayerNames(){
+    for(let pn = 1; pn <= numPlayers; pn++){
+        $('.playerInput').append("<input class='playerNames' id='playerNames' placeholder='Enter Player Name...'></input>")
+    }
+    buildPlayers();
+}
 
 buildFrontNine();
 function buildFrontNine(){
     for(let fn = 1; fn <= frontNine; fn++){
         $(".box").append(`<div id="col${fn}" class="column">${fn}</div>`);
-
-    }
-    addFrontNine();
-    buildBackNine();
-    buildPlayers();
-    
-    
+        
+    }   
 }
 
+addFrontNine();
 function addFrontNine(){
-    $(".box").append('<div id="col${fn}" class="column">Score</div>')
+    $(".box").append('<div id="colAddFront" class="addSeperate">Front</div>')
 
 }
 
+buildBackNine();
 function buildBackNine(){
     for(let bn = 10; bn <= backNine; bn++){
         $(".box").append(`<div id="col${bn}" class="column">${bn}</div>`)
     }
 }
 
+addBackNine();
+function addBackNine(){
+    $(".box").append('<div id="colAddBack" class="addSeperate">Back</div>')
+
+}
+
 function buildPlayers(){
     for(p = 1; p <= numPlayers; p++){
 
         for(let h = 1; h <= backNine; h++){
-        $("#col"+ h).append(`<input id="p${p}h${h}" class="minibox"></input>`);
+            $("#col"+ h).append(`<input id="p${p}h${h}" class="minibox"></input>`);
+            $("#addFront"+ h).append(`<input id="p${p}h${h}" class="minibox"></input>`);
+            $("#addBack"+ h).append(`<input id="p${p}h${h}" class="minibox"></input>`);
+
         }
     }
 }
--- javascript/api.js
+++ javascript/api.js
@@ -1,31 +1,30 @@
 let golfCourses;
 
 (function(){
-    printCourses();
     getCourses();
 })();
 
-function printCourses(){
+function getCourses(){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             golfCourses = JSON.parse(this.responseText);
             console.log(golfCourses);
-            $('.chooseCourses').append('yes');
-
+            displayCourses();
         }
     }
     xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
     xhttp.send();
 }
 
-function getCourses(){
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            $(".chooseCourses").append("x")
-        }
-    }
-    xhttp.open("Get", "https://golf-courses-api.herokuapp.com/courses", true);
-    xhttp.send();
+function displayCourses() {
+    for(let i = 0; i < golfCourses.courses.length; i++) {
+        $('.chooseCourses').append(`
+        <div class="courseCard">
+            <div class="courseImg" style="background-image: 
+            url(${golfCourses.courses[i].image})"></div>
+        <span>${golfCourses.courses[i].name}</span>
+        </div>
+        `)
+    }    
 }
