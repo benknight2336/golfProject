05421d1fe1606c46c22e3aba2b2d10f5bb8327f5
benknight <ben.knight2336@gmail.com>
2019-11-23 18:10:01 -0700

    I have connected and linked the golf course api to the project allowing yards, par, handicap to load. Added some style to kind of get it looing the way i want but it still needs more

--- javascript/action.js
+++ javascript/action.js
@@ -1,43 +1,93 @@
-let numPlayers=0;
-let frontNine = 9;
-let backNine = 18;
 
+let numPlayers=0;
+let frontNine = 8;
+let backNine = 17;
+
+function buildGolfCourseSelect() {
+    console.log(golfCourse)
+    $('.selectTeeTypes').html("");
+    html = `<select class='selectOptions' onchange="buildTeeBoxHoles(this.options[this.selectedIndex].value)">
+    <option>select tee box</option>")`;
+
+    let num = golfCourse.holes[0].teeBoxes.length - 1;
+    for(let t = 0; t <= num; t++) {
+        let teeBox = golfCourse.holes[0].teeBoxes[t];
+        if(teeBox.teeType != "auto change location"){
+            html += `<option value="${t}">${teeBox.teeType}</option>`;
+        }
+    }
+    html += "</select>";
+    $('.selectTeeTypes').html(html);
+    $('.selectTeeTypes').show();
+}
 
 function enterPlayerNames(){
     numPlayers++;
     
-    $('.playerInput').append("<div class='playerNames'><input  id='playerNames' type='text' placeholder='Enter Player Name...'></input></div>")
+    $('.playerInput').append("<div class='playerNames'><input  id='playerNames' type='text' placeholder='Enter Player Name...'></input></div>");
 
     buildPlayers();
 }
 
-buildFrontNine();
-function buildFrontNine(){
-    for(let fn = 1; fn <= frontNine; fn++){
-        $(".box").append(`<div id="col${fn}" class="column">${fn}</div>`);
+function buildTeeBoxHoles(teeBoxIndex){
+    console.log(teeBoxIndex);
+
+    buildFrontNine(teeBoxIndex);
+    addFrontNine();
+    buildBackNine(teeBoxIndex);
+    addBackNine();
+    addTotal();
+    
+}
+
+function buildFrontNine(teeBoxIndex){
+    for(let fn = 0; fn <= frontNine; fn++){
+        teeBoxNum = fn + 1;
+        teeBoxYards = golfCourse.holes[fn].teeBoxes[teeBoxIndex].yards;
+        teeBoxHandicap = golfCourse.holes[fn].teeBoxes[teeBoxIndex].hcp;
+        teeBoxPar = golfCourse.holes[fn].teeBoxes[teeBoxIndex].par;
+        $(".box").append(`<div id="col${teeBoxNum}"                             class="column">
+                        <div>${teeBoxNum}</div>
+                        <div class='yards'>Yards ${teeBoxYards}</div>
+                        <div class ='handicap'> Hcp ${teeBoxHandicap}</div>
+                        <div class ='par'>Par ${teeBoxPar}</div>
+                        </div>
+                        `);
         
     }   
 }
 
-addFrontNine();
 function addFrontNine(){
-        $(".box" ).append('<div id="colAddFront" class="addSeperate">Front</div>')
-    
-
+    $(".box" ).append('<div id="colAddFront" class="addSeperate">Front</div>')
 }
 
-buildBackNine();
-function buildBackNine(){
-    for(let bn = 10; bn <= backNine; bn++){
-        $(".box").append(`<div id="col${bn}" class="column">${bn}</div>`)
+function buildBackNine(teeBoxIndex){
+    for(let bn = 9; bn <= backNine; bn++){
+        teeBoxNum = bn + 1;
+        teeBoxYards = golfCourse.holes[bn].teeBoxes[teeBoxIndex].yards;
+        teeBoxHandicap = golfCourse.holes[bn].teeBoxes[teeBoxIndex].hcp;
+        teeBoxPar = golfCourse.holes[bn].teeBoxes[teeBoxIndex].par;
+
+        $(".box").append(`<div id="col${teeBoxNum}"                             class="column">
+                        <div>${teeBoxNum}</div>
+                        <div class='yards'>Yards ${teeBoxYards}</div>
+                        <div class = 'handicap'> Hcp ${teeBoxHandicap}</div>
+                        <div class ='par'>Par ${teeBoxPar}</div>
+                        </div>`
+                        );
     }
 }
 
-addBackNine();
 function addBackNine(){
     $(".box").append('<div id="colAddBack" class="addSeperate">Back</div>')
 
 }
+
+function addTotal(){
+    $(".box").append('<div id="colAddTotal" class="addTotal">Total</div>')
+
+}
+
 
 function buildPlayers(){
     
@@ -47,6 +97,8 @@ function buildPlayers(){
     }
     $("#colAddFront").append(`<input class="minibox"></input>`);
     $("#colAddBack").append(`<input class="minibox"></input>`);
+    $("#colAddTotal").append(`<input class="minibox"></input>`);
+
     
 }
 
--- javascript/api.js
+++ javascript/api.js
@@ -1,4 +1,5 @@
 let golfCourses;
+let golfCourse;
 
 (function(){
     getCourses();
@@ -17,11 +18,25 @@ function getCourses(){
     xhttp.send();
 }
 
+
+function getCourse(courseId){
+    console.log(courseId);
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+           golfCourse = JSON.parse(this.responseText).data;
+           buildGolfCourseSelect();
+        }
+    }
+    xhttp.open("GET", `https://golf-courses-api.herokuapp.com/courses/${courseId}`, true);
+    xhttp.send();
+} 
+
 function displayCourses() {
     for(let i = 0; i < golfCourses.courses.length; i++) {
         $('.chooseCourses').append(`
         <div class="courseCard">
-            <div class="courseImg" style="background-image: 
+            <div class="courseImg" onclick="getCourse(${golfCourses.courses[i].id})" style="background-image: 
             url(${golfCourses.courses[i].image})"></div>
         <h3>${golfCourses.courses[i].name}</h3>
         </div>
